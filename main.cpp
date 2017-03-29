56546fb08fdd9a45dd2e66e396c7a439ca01b8e0
Kushal S T <utsavts@gmail.com>
2017-03-29 09:56:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
+// C++ program to execute a function before main function
 #include <iostream>
 using namespace std;
 // works only with g++ and gcc compilers
 void beforeMain() __attribute__((constructor));
-//similarly if constructor is replaced with word destructor, function gets executed after main fuction automatically
+// similarly if constructor is replaced with word destructor, function gets executed after main fuction automatically
 void beforeMain()
 {
   cout<<"Gets Executed before Main function"<<endl;
