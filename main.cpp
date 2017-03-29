510c6edd276605b107009db501dc770f6edbeee4
Kushal S T <utsavts@gmail.com>
2017-03-29 09:54:17 +0530

    Add files via upload
    C++ Program to execute a function before main function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// works only with g++ and gcc compilers
+void beforeMain() __attribute__((constructor));
+//similarly if constructor is replaced with word destructor, function gets executed after main fuction automatically
+void beforeMain()
+{
+  cout<<"Gets Executed before Main function"<<endl;
+}
+int main ()
+{
+  cout<<"\nInside main"<<endl;
+  return 0;
+}
